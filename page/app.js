941e35f688388bace5c4a5e89f2f891cb9775d66
Fatmats <melikeats0561@gmail.com>
2021-02-17 16:50:47 +0300

    init

--- page/app.js
+++ page/app.js
@@ -54,26 +54,6 @@ request(myObj).then(data => {
     console.log(error);
 });
 
-function main(r) {
-    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
-    var pi =Math.PI
-    // Print the area of the circle:
-    var area=pi*(r*r)
-    // Print the perimeter of the circle:
-var perimeter=2*pi*r
-
-     console.log(area)
-     console.log(perimeter)
-    try {    
-        // Attempt to redefine the value of constant variable PI
-        PI = 0;
-        // Attempt to print the value of PI
-        console.log(PI);
-    } catch(error) {
-        console.error("You correctly declared 'PI' as a constant.");
-    }
-}
-console.log(main(2,6));
 
 
 
